9648caffd7c6641830920825a1d40f88caa87410
Thomas Montovani <tmontova@umass.edu>
2015-05-01 02:16:08 -0400

    Character moves around the edge and looks toward center

--- Final/src/mygame/Main.java
+++ Final/src/mygame/Main.java
@@ -9,6 +9,7 @@ import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.collision.shapes.CompoundCollisionShape;
 import com.jme3.bullet.collision.shapes.MeshCollisionShape;
+import com.jme3.bullet.control.BetterCharacterControl;
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.input.KeyInput;
@@ -39,6 +40,7 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
     private boolean p1Left=false, p1Right=false, p1Forward=false, p1Back=false;
     private boolean p2Left=false, p2Right=false, p2Forward=false, p2Back=false;
     private Node player;
+    private Torus side;
     
     public static void main(String[] args) {
         Main app = new Main();
@@ -48,7 +50,7 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
     @Override
     public void simpleInitApp() {
         viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
-        cam.setLocation(new Vector3f(0,10,10));
+        cam.setLocation(new Vector3f(0,12,12));
         cam.lookAt(new Vector3f(0,0,0), Vector3f.UNIT_Y);
         flyCam.setEnabled(false);
         setUpKeys();
@@ -60,7 +62,7 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
         
         arenaSetUp();
         playerSetUp();
-        playerSetUp();
+//        playerSetUp();
         setUpLight();
         for(int i = 0; i < 10; i++){
             addBall();
@@ -72,7 +74,6 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
         Sphere s = new Sphere(16, 16, 0.2f);
         Geometry ball_geo = new Geometry("Ball", s);
         ball_geo.setLocalTranslation(new Vector3f(0, 2f, 0f));
-//        ball_geo.setLocalScale(0.2f);
         rootNode.attachChild(ball_geo);
         Material mat2 = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
         ball_geo.setMaterial(mat2);
@@ -86,8 +87,9 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
     
     private void arenaSetUp(){
 
-        Box base = new Box(5, 2, 5);
-        Torus sides = new Torus(16, 16, 1.5f, 6);
+        Box base = new Box(10, 2, 10);
+        Torus sides = new Torus(16, 16, 0.5f, 6);
+        side = sides;
         Geometry base_geo = new Geometry("base", base);
         Geometry sides_geo = new Geometry("side", sides);
 
@@ -107,6 +109,7 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
         base_geo.addControl(base_phy);
         sides_geo.addControl(sides_phy);
         base_phy.setFriction(0.1f);
+        base_phy.setRestitution(0);
         sides_phy.setRestitution(1);
         jBullet.getPhysicsSpace().add(sides_phy);
         jBullet.getPhysicsSpace().add(base_phy);
@@ -116,42 +119,26 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
     }
     
     private void playerSetUp(){
-        Spatial eleph = assetManager.loadModel("Models/eleph2.obj");
-//        Box body = new Box(.5f,.5f, .5f);
-//        Box top = new Box(.2f, .1f, .5f);
-//        Box bottom = new Box(.2f, .01f, .5f);
-//        
-//        Geometry body_geo = new Geometry("Box", body);
-//        Geometry top_geo = new Geometry("Box", top);
-//        Geometry bottom_geo = new Geometry("Box", bottom);
-//        
-//        top_geo.setLocalTranslation(.7f, .5f, 0);
-//        bottom_geo.setLocalTranslation(.7f, .49f, 0);
-//        CollisionShape s = CollisionShapeFactory.createMeshShape(eleph);
-//        CompoundCollisionShape capsule = new CompoundCollisionShape();
-//        capsule.addChildShape(s, Vector3f.ZERO);
+        Spatial eleph = assetManager.loadModel("Models/e1.obj");
+
         CapsuleCollisionShape capsule = new CapsuleCollisionShape(0.5f, 0.5f);
         eleph_phy = new CharacterControl(capsule, .01f);
+        
         player = new Node("player");
  
-        Material eleph_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        eleph_mat.setTexture("ColorMap", assetManager.loadTexture("Textures/elephant.jpg"));
-//        body_geo.setMaterial(eleph_mat);
-//        top_geo.setMaterial(eleph_mat);
-//        bottom_geo.setMaterial(eleph_mat);
+        Material eleph_mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        eleph_mat.setColor("Diffuse", ColorRGBA.Blue);
         eleph.setMaterial(eleph_mat);
-        eleph.setLocalScale(2.5f);
+        eleph.setLocalScale(0.5f);
         
-        player.addControl(eleph_phy);
         player.attachChild(eleph);
-//        player.attachChild(body_geo);
-//        player.attachChild(top_geo);
-//        player.attachChild(bottom_geo);
-        eleph_phy.setGravity(30);
-//        eleph_phy.setPhysicsLocation(new Vector3f(0,0.5f,0));
+        player.addControl(eleph_phy);
+        eleph_phy.setPhysicsLocation(new Vector3f(6,0,0));
+        
+
         jBullet.getPhysicsSpace().add(eleph_phy);
-        char_control.add(eleph_phy);
         rootNode.attachChild(player);
+        char_control.add(eleph_phy);
     }
     
     
@@ -189,41 +176,62 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
         
     }
 
+    private double rotation = 0;
     @Override
     public void simpleUpdate(float tpf) {
         Vector3f pos;
 
         if(p1Left || p1Right || p1Forward || p1Back){
-            System.out.println("SimpleUpdate");
+//            System.out.println("SimpleUpdate");
             pos = char_control.get(0).getPhysicsLocation();
+            float radius = side.getOuterRadius();
+//            System.out.println("Walk direction: "+char_control.get(0).getWalkDirection());
             if(p1Left){
-                char_control.get(0).setPhysicsLocation(new Vector3f(pos.x-.01f, pos.y, pos.z));
+                rotation+=.005;
+                float x = (float) (radius*Math.cos(rotation));
+                float y = pos.y;
+                float z = (float) (radius*Math.sin(rotation));
+                char_control.get(0).setPhysicsLocation(new Vector3f(x, y, z));
+                System.out.println(char_control.get(0).getViewDirection());
+                Vector3f view = new Vector3f(-pos.z, 0, pos.x);
+                view.normalize();
+                char_control.get(0).setViewDirection(view);
+
             }
             if(p1Right){
-                char_control.get(0).setPhysicsLocation(new Vector3f(pos.x+.01f, pos.y, pos.z));
+                rotation-=.005;
+                float x = (float) (radius*Math.cos(rotation));
+                float y = pos.y;
+                float z = (float) (radius*Math.sin(rotation));
+                char_control.get(0).setPhysicsLocation(new Vector3f(x, y, z));
+                System.out.println(char_control.get(0).getViewDirection());
+                Vector3f view = new Vector3f(-pos.z, 0, pos.x);
+                view.normalize();
+                char_control.get(0).setViewDirection(view);
+//                char_control.get(0).setPhysicsLocation(new Vector3f(pos.x+.01f, pos.y, pos.z));
             }
             if(p1Forward){
-                char_control.get(0).setPhysicsLocation(new Vector3f(pos.x, pos.y, pos.z-.01f));
+//                char_control.get(0).setPhysicsLocation(new Vector3f(pos.x, pos.y, pos.z-.01f));
             }
             if(p1Back){
-                char_control.get(0).setPhysicsLocation(new Vector3f(pos.x, pos.y, pos.z+.01f));
+//                char_control.get(0).setPhysicsLocation(new Vector3f(pos.x, pos.y, pos.z+.01f));
             }
         }
-        else if(p2Left || p2Right || p2Forward || p2Back){
-            pos = char_control.get(1).getPhysicsLocation();
-            if(p2Left){
-                char_control.get(1).setPhysicsLocation(new Vector3f(pos.x-.01f, pos.y, pos.z));
-            }
-            if(p2Right){
-                char_control.get(1).setPhysicsLocation(new Vector3f(pos.x+.01f, pos.y, pos.z));
-            }
-            if(p2Forward){
-                char_control.get(1).setPhysicsLocation(new Vector3f(pos.x, pos.y, pos.z-.01f));
-            }
-            if(p2Back){
-                char_control.get(1).setPhysicsLocation(new Vector3f(pos.x, pos.y, pos.z+.01f));
-            }
-        }
+//        else if(p2Left || p2Right || p2Forward || p2Back){
+//            pos = char_control.get(1).getPhysicsLocation();
+//            if(p2Left){
+//                char_control.get(1).setPhysicsLocation(new Vector3f(pos.x-.01f, pos.y, pos.z));
+//            }
+//            if(p2Right){
+//                char_control.get(1).setPhysicsLocation(new Vector3f(pos.x+.01f, pos.y, pos.z));
+//            }
+//            if(p2Forward){
+//                char_control.get(1).setPhysicsLocation(new Vector3f(pos.x, pos.y, pos.z-.01f));
+//            }
+//            if(p2Back){
+//                char_control.get(1).setPhysicsLocation(new Vector3f(pos.x, pos.y, pos.z+.01f));
+//            }
+//        }
     }
 
     @Override
@@ -261,16 +269,16 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
     @Override
     public void collision(PhysicsCollisionEvent event) {
         if(event.getNodeA().getName().equals("Ball")){
-                System.out.println(event.getNodeA()+" "+event.getNodeB());
+//                System.out.println(event.getNodeA()+" "+event.getNodeB());
                 if(event.getNodeB().getName().equals("player")){
-                    System.out.println("Ball has been caught");
+//                    System.out.println("Ball has been caught");
                 }
 
         }
         if(event.getNodeA().getName().equals("player")){
-                System.out.println(event.getNodeA()+" "+event.getNodeB());
+//                System.out.println(event.getNodeA()+" "+event.getNodeB());
                 if(event.getNodeB().getName().equals("Ball")){
-                    System.out.println("Ball has been caught");
+//                    System.out.println("Ball has been caught");
                 }
 
         }
